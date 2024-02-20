2a8dd3e06cbd82607e0c1b56ed38b29a8c378bcb
Calculuslmvt <calculuslmvt@gmail.com>
2024-02-20 14:01:48 +0530

    added data

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const data = {
+    "key1" : "value1",
+    "key2" : "value2"
+}
+
 app.get('/', (req, res) => {
     res.send("Hello world !!!");
 });
@@ -11,6 +16,10 @@ app.get('/', (req, res) => {
 app.get('/route1', (req, res) => {
     res.send("This is route1 ");
 });
+
+app.get('/data', (req, res) => {
+    res.json(data); 
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Port running on ${process.env.PORT}`);
